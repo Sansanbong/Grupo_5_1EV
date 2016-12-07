de715ecbd6ab60301714c332006de2c373ec3fbf
Carlos García <carlos.ga.corpas@gmail.com>
2016-12-07 22:04:39 +0100

    He hecho algo.

---
+++ app/src/main/java/com/android/grupo5/AccesoBBDD.java
@@ -0,0 +1,11 @@
+package com.android.grupo5;
+
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * @author Carlos García y Javier Sánchez
+ */
+
+public class AccesoBBDD {
+    private SQLiteDatabase estonovaasi;
+}
